bd432f2900176ba41e66c22f546821bb9792302d
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2020-11-16 09:31:35 +0200

    Removed old sap store id to store name map (to BERLIN store) from -dev.config

--- config/Shared/config_default-docker_dev.php
+++ config/Shared/config_default-docker_dev.php
@@ -1,11 +1,6 @@
 <?php
 
 use Pyz\Shared\ProductImage\ProductImageConstants;
-use Pyz\Shared\Store\StoreConstants;
-
-$config[StoreConstants::SAP_STORE_ID_TO_STORE_MAP] = [
-    1004 => 'BERLIN',
-];
 
 $config[ProductImageConstants::IMAGES_HOST_URL] = 'https://globus-staging-product-images.s3.eu-central-1.amazonaws.com';
 
