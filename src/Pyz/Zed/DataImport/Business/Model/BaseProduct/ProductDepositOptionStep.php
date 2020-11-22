ac9945dde00296401f9368149c0ea4b6895f0f44
ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
2020-11-22 15:21:04 +0200

    CR fixes

--- src/Pyz/Zed/DataImport/Business/Model/BaseProduct/ProductDepositOptionStep.php
+++ src/Pyz/Zed/DataImport/Business/Model/BaseProduct/ProductDepositOptionStep.php
@@ -62,6 +62,7 @@ class ProductDepositOptionStep extends PublishAwareStep implements DataImportSte
     protected const DEPOSITS_TAX_SET = 'STANDARD';
 
     protected const DEFAULT_DEPOSIT_AMOUNT_MULTIPLIER = 100;
+    protected const DEFAULT_DEPOSIT_COUNT = 1;
 
     /**
      * @var array
@@ -187,7 +188,8 @@ class ProductDepositOptionStep extends PublishAwareStep implements DataImportSte
                 ->filterByIdTaxSet($this->getIdTaxSet(static::DEPOSITS_TAX_SET))
                 ->findOne();
 
-            $grossPrice = ((float)$dataSet[static::KEY_DEPOSIT_AMOUNT] * (int)$dataSet[static::KEY_DEPOSIT_COUNT] ?? 1) * static::DEFAULT_DEPOSIT_AMOUNT_MULTIPLIER;
+            $depositCount = $dataSet[static::KEY_DEPOSIT_COUNT] ?? static::DEFAULT_DEPOSIT_COUNT;
+            $grossPrice = ((float)$dataSet[static::KEY_DEPOSIT_AMOUNT] * (int)$depositCount) * static::DEFAULT_DEPOSIT_AMOUNT_MULTIPLIER;
             $netPrice = $grossPrice / ($taxSetEntity->getSpyTaxRates()[0]->getRate() / 100 + 1);
 
             $priceEntity
