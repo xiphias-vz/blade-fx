816732ba6dc48d9771bbb5fb3bb134304efc3462
pivovarov <stanislav.pivovarov@spryker.com>
2020-11-25 13:14:08 +0200

    OS11SPRYKE-133 Same "Pfand" issue + Bontext fix OS11SPRYKE-94 Cashier export should be automatic

--- src/Pyz/Zed/CashierOrderExport/Business/Aggregator/OrderItemAggregator.php
+++ src/Pyz/Zed/CashierOrderExport/Business/Aggregator/OrderItemAggregator.php
@@ -54,8 +54,6 @@ class OrderItemAggregator implements OrderItemAggregatorInterface
      */
     protected function combineItems(ItemTransfer $itemTransferOne, ItemTransfer $itemTransferTwo): ItemTransfer
     {
-        $itemTransferOne->setQuantity($itemTransferOne->getQuantity() + $itemTransferTwo->getQuantity());
-
-        return $itemTransferOne;
+        return $itemTransferOne->setQuantity($itemTransferOne->getQuantity() + $itemTransferTwo->getQuantity());
     }
 }
