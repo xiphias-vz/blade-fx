69b2adaefb97425b0cd22b11c36056ea278e8b64
MihaelBlazek <66123456+MihaelBlazek@users.noreply.github.com>
2020-12-08 14:21:23 +0100

    OS11SPRYKE-227-Guest order force you to type in your addresss twice (Critical) (#208)
    * Forms fix
    
    * Assigned billing address values to order info
    
    * Assigned billing address values to order info
    
    * Added customer data to quote transfer
    
    * Travis fix
    
    * Added fields and replaced

--- src/Pyz/Yves/CheckoutPage/Process/Steps/AddressStep.php
+++ src/Pyz/Yves/CheckoutPage/Process/Steps/AddressStep.php
@@ -9,6 +9,7 @@ namespace Pyz\Yves\CheckoutPage\Process\Steps;
 
 use Spryker\Shared\Kernel\Transfer\AbstractTransfer;
 use SprykerShop\Yves\CheckoutPage\Process\Steps\AddressStep as SprykerAddressStep;
+use Symfony\Component\HttpFoundation\Request;
 
 class AddressStep extends SprykerAddressStep
 {
@@ -24,5 +25,35 @@ class AddressStep extends SprykerAddressStep
         $data['isBillingSameAsShippingChecked'] = $quoteTransfer->getBillingSameAsShipping();
 
         return $data;
+    }
+
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Generated\Shared\Transfer\QuoteTransfer $quoteTransfer
+     *
+     * @return \Generated\Shared\Transfer\QuoteTransfer
+     */
+    public function execute(Request $request, AbstractTransfer $quoteTransfer)
+    {
+        if ($quoteTransfer->getCustomer() != null && $quoteTransfer->getCustomer()->getIsGuest()) {
+            $billingAddress = $quoteTransfer->getBillingAddress();
+            $newCustomer = $quoteTransfer->getCustomer();
+            $newCustomer->setSalutation($billingAddress->getSalutation());
+            $newCustomer->setFirstName($billingAddress->getFirstName());
+            $newCustomer->setLastName($billingAddress->getLastName());
+            $newCustomer->setZipCode($billingAddress->getZipCode());
+            $newCustomer->setCity($billingAddress->getCity());
+            $newCustomer->setPhone($billingAddress->getPhone());
+            $newCustomer->setMobilePhoneNumber($billingAddress->getCellPhone());
+
+            $quoteTransfer->setCustomer($newCustomer);
+        }
+
+        if (!$this->executeCheckoutAddressStepEnterPreCheckPlugins($quoteTransfer)) {
+            return $quoteTransfer;
+        }
+        $quoteTransfer = $this->stepExecutor->execute($request, $quoteTransfer);
+
+        return $this->calculationClient->recalculate($quoteTransfer);
     }
 }
--- src/Pyz/Yves/CustomerPage/Form/GuestForm.php
+++ src/Pyz/Yves/CustomerPage/Form/GuestForm.php
@@ -9,19 +9,12 @@ namespace Pyz\Yves\CustomerPage\Form;
 
 use SprykerShop\Yves\CustomerPage\Form\GuestForm as SprykerGuestForm;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\Callback;
 use Symfony\Component\Validator\Context\ExecutionContextInterface;
 
 class GuestForm extends SprykerGuestForm
 {
-    public const FIELD_ADDRESS_1 = 'address1';
-    public const FIELD_ADDRESS_2 = 'address2';
-    public const FIELD_ZIP_CODE = 'zip_code';
-    public const FIELD_CITY = 'city';
-    public const FIELD_PHONE = 'phone';
-
     /**
      * @param \Symfony\Component\Form\FormBuilderInterface $builder
      * @param array $options
@@ -31,58 +24,8 @@ class GuestForm extends SprykerGuestForm
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $this
-            ->addSalutationField($builder)
-            ->addFirstNameField($builder)
-            ->addLastNameField($builder)
             ->addEmailField($builder)
-            ->addAddress1Field($builder)
-            ->addAddress2Field($builder)
-            ->addZipCodeField($builder)
-            ->addCityField($builder)
-            ->addPhoneField($builder)
             ->addIsGuestField($builder);
-    }
-
-    /**
-     * @param \Symfony\Component\Form\FormBuilderInterface $builder
-     *
-     * @return \SprykerShop\Yves\CustomerPage\Form\GuestForm
-     */
-    protected function addFirstNameField(FormBuilderInterface $builder)
-    {
-        $builder->add(self::FIELD_FIRST_NAME, TextType::class, [
-            'label' => 'customer.first_name',
-            'constraints' => [
-                $this->createNotBlankConstraint(),
-            ],
-            'attr' =>
-            [
-                'placeholder' => 'customer.first_name',
-            ],
-        ]);
-
-        return $this;
-    }
-
-    /**
-     * @param \Symfony\Component\Form\FormBuilderInterface $builder
-     *
-     * @return \SprykerShop\Yves\CustomerPage\Form\GuestForm
-     */
-    protected function addLastNameField(FormBuilderInterface $builder)
-    {
-        $builder->add(self::FIELD_LAST_NAME, TextType::class, [
-            'label' => 'customer.last_name',
-            'constraints' => [
-                $this->createNotBlankConstraint(),
-            ],
-            'attr' =>
-            [
-                'placeholder' => 'customer.last_name',
-            ],
-        ]);
-
-        return $this;
     }
 
     /**
@@ -111,118 +54,6 @@ class GuestForm extends SprykerGuestForm
                         }
                     },
                 ]),
-            ],
-        ]);
-
-        return $this;
-    }
-
-    /**
-     * @param \Symfony\Component\Form\FormBuilderInterface $builder
-     *
-     * @return \SprykerShop\Yves\CustomerPage\Form\GuestForm
-     */
-    protected function addAddress1Field(FormBuilderInterface $builder)
-    {
-        $builder->add(self::FIELD_ADDRESS_1, TextType::class, [
-            'label' => 'customer.address.address1',
-            'required' => true,
-            'trim' => true,
-            'constraints' => [
-                $this->createNotBlankConstraint(),
-                ProfileForm::createSafeStringRegexConstraint(),
-            ],
-            'attr' => [
-                'placeholder' => 'customer.registration.address1_placeholder',
-            ],
-        ]);
-
-        return $this;
-    }
-
-    /**
-     * @param \Symfony\Component\Form\FormBuilderInterface $builder
-     *
-     * @return \SprykerShop\Yves\CustomerPage\Form\GuestForm
-     */
-    protected function addAddress2Field(FormBuilderInterface $builder)
-    {
-        $builder->add(self::FIELD_ADDRESS_2, TextType::class, [
-            'label' => 'customer.address.number',
-            'required' => true,
-            'trim' => true,
-            'constraints' => [
-                $this->createNotBlankConstraint(),
-            ],
-            'attr' => [
-                'placeholder' => 'customer.registration.address2_placeholder',
-            ],
-        ]);
-
-        return $this;
-    }
-
-    /**
-     * @param \Symfony\Component\Form\FormBuilderInterface $builder
-     *
-     * @return \SprykerShop\Yves\CustomerPage\Form\GuestForm
-     */
-    protected function addZipCodeField(FormBuilderInterface $builder)
-    {
-        $builder->add(self::FIELD_ZIP_CODE, TextType::class, [
-            'label' => 'customer.address.zip_code',
-            'required' => true,
-            'trim' => true,
-            'constraints' => [
-                $this->createNotBlankConstraint(),
-            ],
-            'attr' => [
-                'placeholder' => 'customer.registration.zip_placeholder',
-            ],
-        ]);
-
-        return $this;
-    }
-
-    /**
-     * @param \Symfony\Component\Form\FormBuilderInterface $builder
-     *
-     * @return \SprykerShop\Yves\CustomerPage\Form\GuestForm
-     */
-    protected function addCityField(FormBuilderInterface $builder)
-    {
-        $builder->add(self::FIELD_CITY, TextType::class, [
-            'label' => 'customer.address.city',
-            'required' => true,
-            'trim' => true,
-            'constraints' => [
-                $this->createNotBlankConstraint(),
-                ProfileForm::createSafeStringRegexConstraint(),
-            ],
-            'attr' => [
-                'placeholder' => 'customer.registration.city_placeholder',
-            ],
-        ]);
-
-        return $this;
-    }
-
-    /**
-     * @param \Symfony\Component\Form\FormBuilderInterface $builder
-     *
-     * @return \SprykerShop\Yves\CustomerPage\Form\GuestForm
-     */
-    protected function addPhoneField(FormBuilderInterface $builder)
-    {
-        $builder->add(static::FIELD_PHONE, TextType::class, [
-            'label' => 'customer.phone.checkout.guest',
-            'required' => false,
-            'trim' => true,
-            'constraints' => [
-                ProfileForm::createSafeStringRegexConstraint(),
-            ],
-            'attr' => [
-                'placeholder' => 'customer.phone.checkout.guest',
             ],
         ]);
 
