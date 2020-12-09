7a1b1349b7b690b1a7759bec6e3831f7c0b63c33
Vasily Rodin <RodinVasily@users.noreply.github.com>
2020-12-09 16:57:31 +0100

    OS11SPRYKE-107 Remove prefix by scanning (#228)
    * OS11SPRYKE-107 Remove scanning prefix

--- src/StoreApp/Zed/Picker/Theme/default/components/molecules/form-containers/form-containers.ts
+++ src/StoreApp/Zed/Picker/Theme/default/components/molecules/form-containers/form-containers.ts
@@ -5,26 +5,33 @@ export default class FormContainers extends Component {
     addFormButton: HTMLButtonElement;
     formItemCandidate: string;
     formIndex: number;
+    form: HTMLFormElement;
+
+    protected barcodePrefix: string = '/x11';
 
     protected readyCallback(): void {
         this.containerFormsWrapper = <HTMLElement>document.getElementById('order_item_selection_form_field_sales_order_containers');
         this.addFormButton = <HTMLButtonElement>this.getElementsByClassName(`${this.jsName}__add-container`)[0];
         this.formItemCandidate = this.containerFormsWrapper.dataset.prototype;
         this.formIndex = 0;
+        this.form = <HTMLFormElement>document.getElementsByClassName('js-submit-form-handler__form')[0];
 
         if (this.containerFormsWrapper.innerHTML === "") {
             this.addFormItem();
         }
         this.mapEvents();
+        this.mapEventsForItems();
     }
 
     protected mapEvents(): void {
-        this.addFormButton.addEventListener('click', () => this.onTriggerClick());
+        this.addFormButton.addEventListener('click', () => this.onTriggerAddFormButtonClick());
         this.containerFormsWrapper.addEventListener('click', (event: Event) => this.deleteFormItem(event));
+        this.form.addEventListener('keypress', (event: KeyboardEvent) => this.formKeyPressHandler(event));
     }
 
-    protected onTriggerClick(): void {
+    protected onTriggerAddFormButtonClick(): void {
         this.addFormItem();
+        this.mapEventsForItems();
         this.decorateAdditionalForm();
     }
 
@@ -44,6 +51,28 @@ export default class FormContainers extends Component {
         this.containerFormsWrapper.insertAdjacentHTML('beforeend', formItem);
         this.formIndex += 1;
         this.containerFormsWrapper.dataset.index = String(this.formIndex);
+    }
+
+    protected mapEventsForItems(): void {
+        let formItems = <HTMLElement[]> Array.from(document.getElementsByClassName(`${this.jsName}__form-item`));
+
+        formItems.forEach((element: HTMLInputElement) => {
+            element.addEventListener('input', (event: KeyboardEvent) => this.onFormItemChange(event));
+        });
+    }
+
+    protected formKeyPressHandler(event: KeyboardEvent): void {
+        // Enter key forces the whole form to submit, we want to prevent that for barcode scanner
+        if (event.key == 'Enter') {
+            event.preventDefault();
+        }
+    }
+
+    protected onFormItemChange(event: KeyboardEvent): void {
+        let originalValue = (<HTMLInputElement>event.target).value;
+
+        // force removing of barcode prefix
+        (<HTMLInputElement>event.target).value = originalValue.replace(this.barcodePrefix, '');
     }
 
     protected decorateAdditionalForm(): void {
