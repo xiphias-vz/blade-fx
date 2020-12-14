5c4842d345270d2a2d257b9cb20e277a9f00d7b7
MihaelBlazek <66123456+MihaelBlazek@users.noreply.github.com>
2020-12-14 14:28:59 +0100

    Os11 spryke 251 position overview (Critical) (#244)
    * Added orderPickZone twig
    
    * controller
    
    * Picking zone info
    
    * travis fix

---
+++ src/Pyz/Zed/Sales/Communication/Controller/DetailController.php
@@ -0,0 +1,79 @@
+<?php
+
+/**
+ * This file is part of the Spryker Commerce OS.
+ * For full license information, please view the LICENSE file that was distributed with this source code.
+ */
+
+namespace Pyz\Zed\Sales\Communication\Controller;
+
+use Spryker\Service\UtilText\Model\Url\Url;
+use Spryker\Zed\Sales\Communication\Controller\DetailController as SprykerDetailController;
+use Spryker\Zed\Sales\SalesConfig;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+
+/**
+ * @method \Spryker\Zed\Sales\Communication\SalesCommunicationFactory getFactory()
+ * @method \Spryker\Zed\Sales\Business\SalesFacadeInterface getFacade()
+ * @method \Spryker\Zed\Sales\Persistence\SalesQueryContainerInterface getQueryContainer()
+ * @method \Spryker\Zed\Sales\Persistence\SalesRepositoryInterface getRepository()
+ */
+class DetailController extends SprykerDetailController
+{
+    /**
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     *
+     * @return array|\Symfony\Component\HttpFoundation\RedirectResponse
+     */
+    public function indexAction(Request $request)
+    {
+        $idSalesOrder = $this->castId($request->query->getInt(SalesConfig::PARAM_ID_SALES_ORDER));
+
+        $orderTransfer = $this->getFacade()->findOrderByIdSalesOrder($idSalesOrder);
+
+        if ($orderTransfer === null) {
+            $this->addErrorMessage('Sales order #%d not found.', ['%d' => $idSalesOrder]);
+
+            return $this->redirectResponse(Url::generate('/sales')->build());
+        }
+
+        $distinctOrderStates = $this->getFacade()->getDistinctOrderStates($idSalesOrder);
+        $eventsGroupedByShipment = $this->getFactory()->getOmsFacade()->getGroupedDistinctManualEventsByIdSalesOrder($idSalesOrder);
+        $eventsGroupedByItem = $this->getFactory()->getOmsFacade()->getManualEventsByIdSalesOrder($idSalesOrder);
+        $orderItemSplitFormCollection = $this->getFactory()->createOrderItemSplitFormCollection($orderTransfer->getItems());
+        $events = $this->getFactory()->getOmsFacade()->getDistinctManualEventsByIdSalesOrder($idSalesOrder);
+
+        $blockResponseData = $this->renderSalesDetailBlocks($request, $orderTransfer);
+        if ($blockResponseData instanceof RedirectResponse) {
+            return $blockResponseData;
+        }
+
+        $groupedOrderItems = $this->getFacade()
+            ->getUniqueItemsFromOrder($orderTransfer);
+
+        $pickZones = [];
+        $itemDataArray = [];
+
+        foreach ($groupedOrderItems as $key => $orderItem) {
+            if (!in_array($orderItem->getPickZone(), $pickZones)) {
+                array_push($pickZones, $orderItem->getPickZone());
+            }
+
+            $itemDataArray[array_search($orderItem->getPickZone(), $pickZones)][$key] = $orderItem;
+        }
+
+        return array_merge([
+            'eventsGroupedByItem' => $eventsGroupedByItem,
+            'events' => $events,
+            'eventsGroupedByShipment' => $eventsGroupedByShipment,
+            'distinctOrderStates' => $distinctOrderStates,
+            'order' => $orderTransfer,
+            'orderItemSplitFormCollection' => $orderItemSplitFormCollection,
+            'groupedOrderItems' => $groupedOrderItems,
+            'changeStatusRedirectUrl' => $this->createRedirectLink($idSalesOrder),
+            'pickZones' => $pickZones,
+            'itemDataArray' => $itemDataArray,
+        ], $blockResponseData);
+    }
+}
