fb868678183f968b7dd023a23bb11644068ec9ab
dkrizanec <61667117+dkrizanec@users.noreply.github.com>
2021-01-07 12:52:23 +0100

    321 new version - Catalog fix [Urgent] (#332)
    * Changed csv of the cms_page and cms_page_store.csv
    
    * Removed changes from csv
    
    * Changed position of label one products.
    
    * Centering button in den warenkorb.
    
    * Adding weight calculation fix.
    
    * Added to the repo new structure of product_price
    
    * Changes in product_price.local.csv
    
    * Changes in price csv files.
    
    * Doc block fixes for files.
    
    * Changes in ProductPriceWriterStep.php
    
    * Fetching data without header changes in .csv file.
    
    * Changes for Travis
    
    * Added fixes from comments.
    
    * Sniff fix + changes from comments.
    
    * Writer step fix.
    
    * product saver fix
    
    * product saver fix
    
    * product saver fix
    
    * Fix
    
    * Expander Fix.
    
    * Catalog values fix.
    
    * Commit after code style.

--- src/Pyz/Zed/ProductPageSearch/Business/DataMapper/ProductAbstractSearchDataMapper.php
+++ src/Pyz/Zed/ProductPageSearch/Business/DataMapper/ProductAbstractSearchDataMapper.php
@@ -23,11 +23,25 @@ class ProductAbstractSearchDataMapper extends SprykerProductAbstractSearchDataMa
     {
         $pageMapTransfer = parent::buildPageMap($data, $localeTransfer);
 
+        $einzelgewicht = null;
+        if ($data['attributes']['einzelgewicht']) {
+            $einzelgewicht = $data['attributes']['einzelgewicht'];
+        } elseif ($data['attributes']['einzelgewicht'][0]) {
+            $einzelgewicht = $data['attributes']['einzelgewicht'][0];
+        } else {
+            $einzelgewicht = null;
+        }
+
+        /**
+         * TODO: FETCH CURRENCY
+         */
         $this->pageMapBuilder
             ->addSearchResultData($pageMapTransfer, 'supplier', $data['attributes']['supplier'][0] ?? null)
             ->addSearchResultData($pageMapTransfer, 'grundpreisinhalt', $data['attributes']['grundpreisinhalt'][0] ?? null)
             ->addSearchResultData($pageMapTransfer, 'grundpreismasseinheit', $data['attributes']['grundpreismasseinheit'][0] ?? null)
-            ->addSearchResultData($pageMapTransfer, 'verpackungseinheit', $data['attributes']['verpackungseinheit'][0] ?? null);
+            ->addSearchResultData($pageMapTransfer, 'verpackungseinheit', $data['attributes']['verpackungseinheit'][0] ?? null)
+            ->addSearchResultData($pageMapTransfer, 'einzelgewicht', $einzelgewicht)
+            ->addSearchResultData($pageMapTransfer, 'pricePerKg', $data['prices']['EUR']['PRICE_PER_KG']['DEFAULT'] ?? null);
 
         return $pageMapTransfer;
     }
