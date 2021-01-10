41e0356d2f6cdbf124500507b7ce562485621389
dkrizanec <61667117+dkrizanec@users.noreply.github.com>
2021-01-10 10:52:26 +0100

    321 new version fix 2.0 (#342)
    * Changed csv of the cms_page and cms_page_store.csv
    
    * Removed changes from csv
    
    * Changed position of label one products.
    
    * Centering button in den warenkorb.
    
    * Adding weight calculation fix.
    
    * Added to the repo new structure of product_price
    
    * Changes in product_price.local.csv
    
    * Changes in price csv files.
    
    * Doc block fixes for files.
    
    * Changes in ProductPriceWriterStep.php
    
    * Fetching data without header changes in .csv file.
    
    * Changes for Travis
    
    * Added fixes from comments.
    
    * Sniff fix + changes from comments.
    
    * Writer step fix.
    
    * product saver fix
    
    * product saver fix
    
    * product saver fix
    
    * Fix
    
    * Expander Fix.
    
    * Catalog values fix.
    
    * Commit after code style.
    
    * Einzelgewicht bug fixed in ProductAbstractSearchDataMapper.php.
    
    * Einzelgewicht bug fixed in ProductAbstractSearchDataMapper.php.
    
    * Mapper fix.
    
    * Mapper fix.
    
    * Mapper fix.

--- src/Pyz/Zed/ProductPageSearch/Business/DataMapper/ProductAbstractSearchDataMapper.php
+++ src/Pyz/Zed/ProductPageSearch/Business/DataMapper/ProductAbstractSearchDataMapper.php
@@ -10,6 +10,7 @@ namespace Pyz\Zed\ProductPageSearch\Business\DataMapper;
 use Generated\Shared\Transfer\LocaleTransfer;
 use Generated\Shared\Transfer\PageMapTransfer;
 use Spryker\Zed\ProductPageSearch\Business\DataMapper\ProductAbstractSearchDataMapper as SprykerProductAbstractSearchDataMapper;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
 class ProductAbstractSearchDataMapper extends SprykerProductAbstractSearchDataMapper
 {
@@ -24,27 +25,46 @@ class ProductAbstractSearchDataMapper extends SprykerProductAbstractSearchDataMa
         $pageMapTransfer = parent::buildPageMap($data, $localeTransfer);
 
         $einzelgewicht = null;
-        if (isset($data['attributes']['einzelgewicht'])) {
-            $einzelgewicht = $data['attributes']['einzelgewicht'];
-        } elseif (isset($data['attributes']['einzelgewicht'][0])) {
-            $einzelgewicht = $data['attributes']['einzelgewicht'][0];
-        } else {
-            $einzelgewicht = null;
-        }
-
         $pricePerKg = null;
-        if (isset($data['prices']['EUR']['PRICE_PER_KG']['DEFAULT'])) {
-            $pricePerKg = $data['prices']['EUR']['PRICE_PER_KG']['DEFAULT'];
+
+        try {
+            if (isset($data['attributes']['einzelgewicht'])) {
+                $einzelgewicht = $data['attributes']['einzelgewicht'];
+            } elseif (isset($data['attributes']['einzelgewicht'][0])) {
+                $einzelgewicht = $data['attributes']['einzelgewicht'][0];
+            } else {
+                $einzelgewicht = null;
+            }
+
+            /**
+             * TODO: FETCH CURRENCY
+             */
+
+            if (isset($data['prices']['EUR']['PRICE_PER_KG']['DEFAULT'])) {
+                $pricePerKg = $data['prices']['EUR']['PRICE_PER_KG']['DEFAULT'];
+            }
+        } catch (Exception $e) {
         }
 
-        /**
-         * TODO: FETCH CURRENCY
-         */
-        $this->pageMapBuilder
-            ->addSearchResultData($pageMapTransfer, 'supplier', $data['attributes']['supplier'][0] ?? null)
-            ->addSearchResultData($pageMapTransfer, 'grundpreisinhalt', $data['attributes']['grundpreisinhalt'][0] ?? null)
-            ->addSearchResultData($pageMapTransfer, 'grundpreismasseinheit', $data['attributes']['grundpreismasseinheit'][0] ?? null)
-            ->addSearchResultData($pageMapTransfer, 'verpackungseinheit', $data['attributes']['verpackungseinheit'][0] ?? null);
+        $supplier = null;
+        if (isset($data['attributes']['supplier'][0])) {
+            $supplier = $data['attributes']['supplier'][0];
+        }
+
+        $grundpreisinhalt = null;
+        if (isset($data['attributes']['grundpreisinhalt'][0])) {
+            $grundpreisinhalt = $data['attributes']['grundpreisinhalt'][0];
+        }
+
+        $grundpreismasseinheit = null;
+        if (isset($data['attributes']['grundpreismasseinheit'][0])) {
+            $grundpreismasseinheit = $data['attributes']['grundpreismasseinheit'][0];
+        }
+
+        $verpackungseinheit = null;
+        if (isset($data['attributes']['verpackungseinheit'][0])) {
+            $verpackungseinheit = $data['attributes']['verpackungseinheit'][0];
+        }
 
         if ($einzelgewicht != null) {
             $this->pageMapBuilder
@@ -54,6 +74,26 @@ class ProductAbstractSearchDataMapper extends SprykerProductAbstractSearchDataMa
         if ($pricePerKg != null) {
             $this->pageMapBuilder
                 ->addSearchResultData($pageMapTransfer, 'pricePerKg', $pricePerKg);
+        }
+
+        if ($supplier != null) {
+            $this->pageMapBuilder
+                ->addSearchResultData($pageMapTransfer, 'supplier', $supplier);
+        }
+
+        if ($grundpreisinhalt != null) {
+            $this->pageMapBuilder
+                ->addSearchResultData($pageMapTransfer, 'grundpreisinhalt', $grundpreisinhalt);
+        }
+
+        if ($grundpreismasseinheit != null) {
+            $this->pageMapBuilder
+                ->addSearchResultData($pageMapTransfer, 'grundpreismasseinheit', $grundpreismasseinheit);
+        }
+
+        if ($verpackungseinheit != null) {
+            $this->pageMapBuilder
+                ->addSearchResultData($pageMapTransfer, 'verpackungseinheit', $verpackungseinheit);
         }
 
         return $pageMapTransfer;
