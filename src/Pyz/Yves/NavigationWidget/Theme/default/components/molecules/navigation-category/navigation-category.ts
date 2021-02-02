e3bb1de610568484502569bf0062b4f415576dde
Emanuel Dajti <66240866+DajtiEmanuel@users.noreply.github.com>
2021-02-02 08:53:45 +0100

    Os11 spryke 322 - Added outlet for sales category in filter only (#474)
    * for testing purposes
    
    * revert
    
    * config
    
    * Added missing characteristics in product controller
    
    * Added outlet attribute to category name links
    
    * Added outlet for  sales category in  filter only
    
    Co-authored-by: ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
    Co-authored-by: EmanuelDajti-kps <emanuel.dajti@kps.com>

--- src/Pyz/Yves/NavigationWidget/Theme/default/components/molecules/navigation-category/navigation-category.ts
+++ src/Pyz/Yves/NavigationWidget/Theme/default/components/molecules/navigation-category/navigation-category.ts
@@ -1,17 +1,16 @@
 window.addEventListener('DOMContentLoaded', (e) => {
-    const navigationList = document.querySelector('#product-category-list');
-    if(!navigationList) {
+    const filterSection = document.querySelector('.filter-section');
+    if(!filterSection) {
         return;
     }
-    const navigationLinks = navigationList.querySelectorAll('.navigation-multilevel-node__link--lvl-1 a');
     let currentUrl = location.pathname;
 
     if(currentUrl.includes('/de/outlet')) {
-        for(let i = 0; i < navigationLinks.length; i++) {
-            let categoryName = navigationLinks[i].href.substring(navigationLinks[i].href.lastIndexOf('/') + 1);
-            if(categoryName !== 'outlet') {
-                navigationLinks[i].href = '/de/outlet' +'/'+ categoryName;
-            }
+        const filterCategoryLink = filterSection.querySelectorAll('.filter-category .filter-category__item--level-1 .filter-category__link');
+        for(let i = 0; i < filterCategoryLink.length; i++) {
+            let urlCategoryLink = filterCategoryLink[i].getAttribute('data-url');
+            let categorySalesLink = '/de/outlet/'+ urlCategoryLink.substring(urlCategoryLink.indexOf("/") + 4);
+            filterCategoryLink[i].setAttribute('data-url', categorySalesLink);
         }
     }
 })
