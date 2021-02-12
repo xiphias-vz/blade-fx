fb2a011edadd9c2ff57248c80398296540630fec
Emanuel Dajti <66240866+DajtiEmanuel@users.noreply.github.com>
2021-02-12 18:00:04 +0100

    OS11SPRYKE-401 no error message shown for not available items fix (#526)
    * for testing purposes
    
    * revert
    
    * config
    
    * Added missing characteristics in product controller
    
    * Availablity cart items handler
    
    * Revert changes on develop
    
    * gitignore
    
    * change gitignore file
    
    * Revert csv changes
    
    * Revert config changes
    
    * Added error message for not valid items
    
    Co-authored-by: ruslan.dovhospynyi <ruslan.dovhospynyi@spryker.com>
    Co-authored-by: EmanuelDajti-kps <emanuel.dajti@kps.com>

--- src/Pyz/Zed/AvailabilityCartConnector/Business/Cart/CheckCartAvailability.php
+++ src/Pyz/Zed/AvailabilityCartConnector/Business/Cart/CheckCartAvailability.php
@@ -10,12 +10,15 @@ namespace Pyz\Zed\AvailabilityCartConnector\Business\Cart;
 use ArrayObject;
 use Generated\Shared\Transfer\CartChangeTransfer;
 use Generated\Shared\Transfer\CartPreCheckResponseTransfer;
+use Generated\Shared\Transfer\MessageTransfer;
 use Generated\Shared\Transfer\ProductAvailabilityCriteriaTransfer;
 use Spryker\DecimalObject\Decimal;
 use Spryker\Zed\AvailabilityCartConnector\Business\Cart\CheckCartAvailability as SpyCheckCartAvailability;
 
 class CheckCartAvailability extends SpyCheckCartAvailability
 {
+    public const CART_PRE_CHECK_AVAILABILITY_NOT_SELLABLE = 'cart.pre.check.availability.failed.not.sellable';
+
     /**
      * @param \Generated\Shared\Transfer\CartChangeTransfer $cartChangeTransfer
      *
@@ -58,9 +61,12 @@ class CheckCartAvailability extends SpyCheckCartAvailability
 
             if (!$isSellable) {
                 $cartPreCheckResponseTransfer->setIsSuccess(false);
+                $availability = $this->findProductConcreteAvailability($itemTransfer, $storeTransfer, $productAvailabilityCriteriaTransfer);
+
                 if ($action != 'add-valid-items') {
-                    $availability = $this->findProductConcreteAvailability($itemTransfer, $storeTransfer, $productAvailabilityCriteriaTransfer);
                     $messages[] = $this->createItemIsNotAvailableMessageTransfer($availability, $itemTransfer->getSku());
+                } else {
+                    $messages[] = $this->createItemIsNotAvailableMessageTransferForNotSellableItems($availability, $itemTransfer->getSku());
                 }
             }
 
@@ -70,5 +76,40 @@ class CheckCartAvailability extends SpyCheckCartAvailability
         $cartPreCheckResponseTransfer->setMessages($messages);
 
         return $cartPreCheckResponseTransfer;
+    }
+
+    /**
+     * @param \Spryker\DecimalObject\Decimal $availability
+     * @param string $sku
+     *
+     * @return \Generated\Shared\Transfer\MessageTransfer
+     */
+    protected function createItemIsNotAvailableMessageTransferForNotSellableItems(Decimal $availability, string $sku): MessageTransfer
+    {
+        $translationKey = $this->getTranslationKeyForDeletedItems($availability);
+
+        $messageTransfer = new MessageTransfer();
+        $messageTransfer->setValue($translationKey);
+        $messageTransfer->setParameters([
+            static::STOCK_TRANSLATION_PARAMETER => $availability->trim()->toString(),
+            static::SKU_TRANSLATION_PARAMETER => $sku,
+        ]);
+
+        return $messageTransfer;
+    }
+
+    /**
+     * @param \Spryker\DecimalObject\Decimal $availability
+     *
+     * @return string
+     */
+    protected function getTranslationKeyForDeletedItems(Decimal $availability): string
+    {
+        $translationKey = static::CART_PRE_CHECK_AVAILABILITY_NOT_SELLABLE;
+        if ($availability->lessThanOrEquals(0)) {
+            $translationKey = static::CART_PRE_CHECK_AVAILABILITY_NOT_SELLABLE;
+        }
+
+        return $translationKey;
     }
 }
