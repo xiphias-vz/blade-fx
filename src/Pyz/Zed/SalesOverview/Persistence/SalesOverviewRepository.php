7041de108f56a46d7a7636a76c6d44a6890daa0f
Marin Jelinek <74537137+mjelinekkps@users.noreply.github.com>
2021-03-31 17:36:13 +0200

    OS11SPRYKE-32 picking progress overivew (#686)
    * Changes for the backoffice overview.
    
    * Working on filters.
    
    * Persistance added
    
    * Working on filters.
    
    * query added 1. version
    
    * Added choice and total
    
    * added totals
    
    * Adding data to the table.
    
    * Travis fix.
    
    * Added CSS.
    
    * Added EIN as default store.
    
    * Changed label
    
    * added totals for order
    
    * added totals for order
    
    * Removed ready for picking state in one zone.
    
    * added totals for order
    
    Co-authored-by: dkrizanec <domagoj.krizanec@gmail.com>

--- src/Pyz/Zed/SalesOverview/Persistence/SalesOverviewRepository.php
+++ src/Pyz/Zed/SalesOverview/Persistence/SalesOverviewRepository.php
@@ -36,8 +36,15 @@ class SalesOverviewRepository extends AbstractRepository implements SalesOvervie
         $result = [];
         foreach ($data as $item) {
             $state = $item["status"];
-            foreach ($statesToShow as $stateDef) {
-                $result[$item["timeSlot"]][$item["pickZone"]][$stateDef] = 0;
+            if (!array_key_exists($item["timeSlot"], $result)) {
+                foreach ($statesToShow as $stateDef) {
+                    $result[$item["timeSlot"]]["Total"][$stateDef] = 0;
+                }
+            }
+            if (!array_key_exists($item["pickZone"], $result[$item["timeSlot"]])) {
+                foreach ($statesToShow as $stateDef) {
+                    $result[$item["timeSlot"]][$item["pickZone"]][$stateDef] = 0;
+                }
             }
             switch ($choice) {
                 case 1:
@@ -58,6 +65,9 @@ class SalesOverviewRepository extends AbstractRepository implements SalesOvervie
             foreach ($timeSlotValue as $pickZoneKey => $pickZoneValue) {
                 if (!array_key_exists($pickZoneKey, $result["Total"])) {
                     $result["Total"][$pickZoneKey] = [];
+                    foreach ($statesToShow as $stateDef) {
+                        $result["Total"][$pickZoneKey][$stateDef] = 0;
+                    }
                 }
                 foreach ($pickZoneValue as $stateKey => $stateValue) {
                     if (!array_key_exists($stateKey, $result["Total"][$pickZoneKey])) {
@@ -84,71 +94,49 @@ class SalesOverviewRepository extends AbstractRepository implements SalesOvervie
              , right(sss.requested_delivery_date, 11) as timeSlot
              , date_format(left(sss.requested_delivery_date, 10), '%d.%m.%Y') as deliveryDate
              , ssoi.pick_zone as pickZone
-             , case when popb.fk_picking_zone is null then sit.name else
-             	case when sit.name = '" . OmsConfig::STORE_STATE_READY_FOR_PICKING . "' then 'in picking' else sit.name end
-               end as status
-             , sso2.orderCount as zoneOrdersCount
+             , case when not popb.fk_sales_order is null AND not ssoi1.fk_sales_order IS NULL then 'in picking' else sit.name end as status
              , count(distinct sso.order_reference) as ordersCount
              , count(distinct ssoi.sku) as orderItemsCount
              , sum(ssoi.quantity) as orderItemsQuantity
              , sum(ssoi.new_weight) as orderItemsWeight
         from spy_sales_order sso
                  inner join spy_sales_order_item ssoi on sso.id_sales_order = ssoi.fk_sales_order
+                 left outer join spy_sales_order_item ssoi1 on sso.id_sales_order = ssoi1.fk_sales_order AND ssoi.pick_zone = ssoi1.pick_zone
+                 	and ssoi1.fk_oms_order_item_state = (select id_oms_order_item_state from spy_oms_order_item_state where name = '" . OmsConfig::STORE_STATE_READY_FOR_PICKING . "')
                  left outer join spy_sales_shipment sss on sso.id_sales_order = sss.fk_sales_order
                  inner join pyz_picking_zone ppz on ssoi.pick_zone = ppz.name
-                 left outer join pyz_order_picking_block popb on popb.fk_sales_order = sso.id_sales_order and ppz.id_picking_zone = popb.fk_picking_zone
+                 left outer join (select fk_sales_order from pyz_order_picking_block group by fk_sales_order) popb on popb.fk_sales_order = sso.id_sales_order
                  inner join spy_oms_order_item_state sit on ssoi.fk_oms_order_item_state = sit.id_oms_order_item_state
-            inner join (
-                select sss1.requested_delivery_date, ssoi1.pick_zone, sso1.merchant_reference,
-                    count(distinct sso1.order_reference) as orderCount
-                from spy_sales_order sso1
-                    inner join spy_sales_order_item ssoi1 on sso1.id_sales_order = ssoi1.fk_sales_order
-                    left outer join spy_sales_shipment sss1 on sso1.id_sales_order = sss1.fk_sales_order
-                where sss1.requested_delivery_date like '" . $deliveryDate . "%'
-                    and sso1.merchant_reference = '" . $merchantReference . "'
-                group by sss1.requested_delivery_date, ssoi1.pick_zone, sso1.merchant_reference
-            ) sso2 on sss.requested_delivery_date = sso2.requested_delivery_date
-                     and ssoi.pick_zone = sso2.pick_zone and sso.merchant_reference = sso2.merchant_reference
+         where sss.requested_delivery_date like '" . $deliveryDate . "%'
+                    and sso.merchant_reference = '" . $merchantReference . "'
         group by
             sss.requested_delivery_date
             , ssoi.pick_zone
             , status
-            , sso2.orderCount
         union
-        select
+		select
             sss.requested_delivery_date
              , right(sss.requested_delivery_date, 11) as timeSlot
              , date_format(left(sss.requested_delivery_date, 10), '%d.%m.%Y') as deliveryDate
              , 'Total' as pickZone
-             , case when popb.fk_picking_zone is null then sit.name else
-             	case when sit.name = '" . OmsConfig::STORE_STATE_READY_FOR_PICKING . "' then 'in picking' else sit.name end
-               end as status
-             , sso2.orderCount as zoneOrdersCount
+             , case when not popb.fk_sales_order is null AND not ssoi1.fk_sales_order IS NULL then 'in picking' else sit.name end as status
              , count(distinct sso.order_reference) as ordersCount
              , count(distinct ssoi.sku) as orderItemsCount
              , sum(ssoi.quantity) as orderItemsQuantity
              , sum(ssoi.new_weight) as orderItemsWeight
         from spy_sales_order sso
                  inner join spy_sales_order_item ssoi on sso.id_sales_order = ssoi.fk_sales_order
+                 left outer join spy_sales_order_item ssoi1 on sso.id_sales_order = ssoi1.fk_sales_order AND ssoi.pick_zone = ssoi1.pick_zone
+                 	and ssoi1.fk_oms_order_item_state = (select id_oms_order_item_state from spy_oms_order_item_state where name = '" . OmsConfig::STORE_STATE_READY_FOR_PICKING . "')
                  left outer join spy_sales_shipment sss on sso.id_sales_order = sss.fk_sales_order
                  inner join pyz_picking_zone ppz on ssoi.pick_zone = ppz.name
-                 left outer join pyz_order_picking_block popb on popb.fk_sales_order = sso.id_sales_order and ppz.id_picking_zone = popb.fk_picking_zone
+                 left outer join (select fk_sales_order from pyz_order_picking_block group by fk_sales_order) popb on popb.fk_sales_order = sso.id_sales_order
                  inner join spy_oms_order_item_state sit on ssoi.fk_oms_order_item_state = sit.id_oms_order_item_state
-            inner join (
-                select sss1.requested_delivery_date, ssoi1.pick_zone, sso1.merchant_reference,
-                    count(distinct sso1.order_reference) as orderCount
-                from spy_sales_order sso1
-                    inner join spy_sales_order_item ssoi1 on sso1.id_sales_order = ssoi1.fk_sales_order
-                    left outer join spy_sales_shipment sss1 on sso1.id_sales_order = sss1.fk_sales_order
-                where sss1.requested_delivery_date like '" . $deliveryDate . "%'
-                    and sso1.merchant_reference = '" . $merchantReference . "'
-                group by sss1.requested_delivery_date, ssoi1.pick_zone, sso1.merchant_reference
-            ) sso2 on sss.requested_delivery_date = sso2.requested_delivery_date
-                     and ssoi.pick_zone = sso2.pick_zone and sso.merchant_reference = sso2.merchant_reference
+         where sss.requested_delivery_date like '" . $deliveryDate . "%'
+                    and sso.merchant_reference = '" . $merchantReference . "'
         group by
             sss.requested_delivery_date
-            , status
-            , sso2.orderCount";
+            , status";
 
         return $this->getResult($sql, true);
     }
