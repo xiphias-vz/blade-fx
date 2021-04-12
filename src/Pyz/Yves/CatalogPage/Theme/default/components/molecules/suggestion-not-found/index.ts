a165cee33d9db9a43ead57fe9e50d7d11ace6f88
MihaelBlazek <66123456+MihaelBlazek@users.noreply.github.com>
2021-04-12 10:10:24 +0200

    OS11SPRYKE-491 Extend Analytics: Event Zero Results Search (#707)
    * Starting to work on suggestions.
    
    * Extending GA logic.
    
    * Removing script.
    
    * Added on change zeoResultsSearch datalayer push
    
    Co-authored-by: dkrizanec <domagoj.krizanec@gmail.com>

---
+++ src/Pyz/Yves/CatalogPage/Theme/default/components/molecules/suggestion-not-found/index.ts
@@ -0,0 +1,3 @@
+import './suggestion-not-found';
+import register from 'ShopUi/app/registry';
+export default register('suggestion-not-found', () => import(/* webpackMode: "eager" */'./suggestion-not-found'));
---
+++ src/Pyz/Yves/CatalogPage/Theme/default/components/molecules/suggestion-not-found/suggestion-not-found.ts
@@ -0,0 +1,14 @@
+import Component from 'ShopUi/models/component';
+
+export default class SuggestionNotFound extends Component {
+
+    protected readyCallback(): void {}
+
+    protected init(): void {
+    }
+
+    protected mapEvents(): void {
+
+    }
+
+}
