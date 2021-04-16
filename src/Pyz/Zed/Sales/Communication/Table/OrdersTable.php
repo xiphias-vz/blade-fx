5c8ae6d6b864a2e4fccf07487b988e4f0b79200a
GordanaZeljezic <77142794+GordanaZeljezic@users.noreply.github.com>
2021-04-16 09:14:43 +0200

    OS11SPRYKE-475 Synchronize CDC and Spryker phone number information (#738)
    * Update shelfs after import
    
    * travis fix.
    
    * travis fix.
    
    * travis fix.
    
    * travis fix.
    
    * travis fix.
    
    * Get stock over merchant
    
    * OS11SPRYKE-475 Synchronize CDC and Spryker phone number information
    
    * added mobile phone
    
    * Changed variable value of hasShippingAddresses
    
    * Changed hasShippingAddresses value
    
    * address twig
    
    * added mobile phone
    
    * update customer info
    
    * Changed logic.
    
    * Code sniff fix
    
    * store mobile phone in CDC
    
    * Changed address.twig
    
    * Remove comment from CustomerUserProvider.php
    
    * code sniff fix
    
    * Added mobile field in table
    
    Co-authored-by: dkrizanec <domagoj.krizanec@gmail.com>
    Co-authored-by: mjelinekkps <marin.jelinek@kps.com>

--- src/Pyz/Zed/Sales/Communication/Table/OrdersTable.php
+++ src/Pyz/Zed/Sales/Communication/Table/OrdersTable.php
@@ -173,6 +173,7 @@ class OrdersTable extends SprykerOrdersTable
             SpySalesOrderTableMap::COL_CUSTOMER_REFERENCE => 'Customer Full Name',
             SpySalesOrderTableMap::COL_EMAIL => 'Email',
             static::ADDRESS_PHONE => 'Phone',
+            SpySalesOrderAddressTableMap::COL_CELL_PHONE => 'Mobile',
             static::ITEM_STATE_NAMES_CSV => 'Order State',
             static::GRAND_TOTAL => 'Grand Total',
             static::NUMBER_OF_ORDER_ITEMS => 'Number of Items',
@@ -263,6 +264,7 @@ class OrdersTable extends SprykerOrdersTable
                 SpySalesOrderTableMap::COL_CUSTOMER_REFERENCE => $this->formatCustomer($item),
                 SpySalesOrderTableMap::COL_EMAIL => $this->formatEmailAddress($item[SpySalesOrderTableMap::COL_EMAIL]),
                 static::ADDRESS_PHONE => $this->getAddressPhone($item),
+                SpySalesOrderAddressTableMap::COL_CELL_PHONE => $this->getAddressMobilePhone($item),
                 static::ITEM_STATE_NAMES_CSV => $this->groupItemStateNames($item[OrdersTableQueryBuilder::FIELD_ITEM_STATE_NAMES_CSV]),
                 static::GRAND_TOTAL => $this->getGrandTotal($item),
                 static::NUMBER_OF_ORDER_ITEMS => $item[OrdersTableQueryBuilder::FIELD_NUMBER_OF_ORDER_ITEMS],
@@ -305,6 +307,16 @@ class OrdersTable extends SprykerOrdersTable
     protected function getAddressPhone(array $item): string
     {
         return $item[static::ADDRESS_PHONE][SpySalesOrderAddressTableMap::COL_PHONE] ?? 'No Billing address or Phone was set';
+    }
+
+    /**
+     * @param array $item
+     *
+     * @return string
+     */
+    protected function getAddressMobilePhone(array $item): string
+    {
+        return $item[static::ADDRESS_PHONE][SpySalesOrderAddressTableMap::COL_CELL_PHONE] ?? 'No Billing address or Phone was set';
     }
 
     /**
