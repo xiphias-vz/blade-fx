2ffcf80d0a92806f53e02123cf11566a0af8007d
Marin Jelinek <74537137+mjelinekkps@users.noreply.github.com>
2021-05-11 16:53:23 +0200

    Check CDC data before update (#821)

--- src/Pyz/Yves/CustomerPage/Plugin/Application/CustomerTransferCustom.php
+++ src/Pyz/Yves/CustomerPage/Plugin/Application/CustomerTransferCustom.php
@@ -33,45 +33,46 @@ class CustomerTransferCustom
             $data = JSON::parse($eventData);
         }
 
-        $customerTransfer->setEmail($data["profile"]["email"])
-            ->setFirstName($this->getValue($data["profile"], "firstName"))
-            ->setLastName($this->getValue($data["profile"], "lastName"))
-            ->setCity($this->getValue($data["profile"], "city"))
-            ->setZipCode($this->getValue($data["profile"], "zip"))
-            ->setBirthDay($this->getValue($data["profile"], "birthDay"))
-            ->setBirthMonth($this->getValue($data["profile"], "birthMonth"))
-            ->setBirthYear($this->getValue($data["profile"], "birthYear"))
-            ->setDateOfBirth($data["profile"]["birthYear"] . '-' . $data["profile"]["birthMonth"] . '-' . $data["profile"]["birthDay"])
-            ->setUsername($this->getValue($data["profile"], "email"))
-            ->setGender($this->getMapGender($this->getValue($data["profile"], "gender")))
-            ->setSalutation($this->getMapSalutation($this->getValue($data["data"], "salutation")))
-            ->setAddress1($this->getValue($data["data"]["address"], "street"))
-            ->setAddress2($this->getValue($data["data"]["address"], "houseNo"))
-            ->setCity($this->getValue($data["profile"], "city"))
-            ->setZipCode($this->getValue($data["profile"], "zip"))
-            ->setCountry($this->getValue($data["profile"], "country"));
+        $city = $this->getValue($data["profile"], "city");
+        if (!empty($city)) {
+            $customerTransfer->setEmail($data["profile"]["email"])
+                ->setFirstName($this->getValue($data["profile"], "firstName"))
+                ->setLastName($this->getValue($data["profile"], "lastName"))
+                ->setCity($this->getValue($data["profile"], "city"))
+                ->setZipCode($this->getValue($data["profile"], "zip"))
+                ->setBirthDay($this->getValue($data["profile"], "birthDay"))
+                ->setBirthMonth($this->getValue($data["profile"], "birthMonth"))
+                ->setBirthYear($this->getValue($data["profile"], "birthYear"))
+                ->setDateOfBirth($data["profile"]["birthYear"] . '-' . $data["profile"]["birthMonth"] . '-' . $data["profile"]["birthDay"])
+                ->setUsername($this->getValue($data["profile"], "email"))
+                ->setGender($this->getMapGender($this->getValue($data["profile"], "gender")))
+                ->setSalutation($this->getMapSalutation($this->getValue($data["data"], "salutation")))
+                ->setAddress1($this->getValue($data["data"]["address"], "street"))
+                ->setAddress2($this->getValue($data["data"]["address"], "houseNo"))
+                ->setCountry($this->getValue($data["profile"], "country"));
 
-        if (array_key_exists("cardID", $data["data"])) {
-            $customerTransfer->setMyGlobusCard($data["data"]["cardID"]);
-        }
+            if (array_key_exists("cardID", $data["data"])) {
+                $customerTransfer->setMyGlobusCard($data["data"]["cardID"]);
+            }
 
-        if (array_key_exists("preferences", $data)) {
-            if (array_key_exists("terms.meinGlobus", $data["preferences"])) {
-                $customerTransfer->setThirdPartyRegistration($data["preferences"]["terms.meinGlobus"]["isConsentGranted"]);
-            } elseif (array_key_exists("terms", $data["preferences"])) {
-                if (array_key_exists("meinGlobus", $data["preferences"]["terms"])) {
-                    $customerTransfer->setThirdPartyRegistration($data["preferences"]["terms"]["meinGlobus"]["isConsentGranted"]);
+            if (array_key_exists("preferences", $data)) {
+                if (array_key_exists("terms.meinGlobus", $data["preferences"])) {
+                    $customerTransfer->setThirdPartyRegistration($data["preferences"]["terms.meinGlobus"]["isConsentGranted"]);
+                } elseif (array_key_exists("terms", $data["preferences"])) {
+                    if (array_key_exists("meinGlobus", $data["preferences"]["terms"])) {
+                        $customerTransfer->setThirdPartyRegistration($data["preferences"]["terms"]["meinGlobus"]["isConsentGranted"]);
+                    }
                 }
             }
-        }
 
-        if (array_key_exists("phones", $data["profile"])) {
-            $phones = $data["profile"]["phones"];
-            foreach ($phones as $phone) {
-                if ($phone["type"] == "home") {
-                    $customerTransfer->setPhone($phone["number"]);
-                } else {
-                    $customerTransfer->setMobilePhoneNumber($phone["number"]);
+            if (array_key_exists("phones", $data["profile"])) {
+                $phones = $data["profile"]["phones"];
+                foreach ($phones as $phone) {
+                    if ($phone["type"] == "home") {
+                        $customerTransfer->setPhone($phone["number"]);
+                    } else {
+                        $customerTransfer->setMobilePhoneNumber($phone["number"]);
+                    }
                 }
             }
         }
