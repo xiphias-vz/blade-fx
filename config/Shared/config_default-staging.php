24a5b91c11e46817db6bb690f6446d40365b9ad0
dkrizanec <61667117+dkrizanec@users.noreply.github.com>
2021-06-23 13:34:06 +0200

    OS11SPRYKE-685 -> CZ change in config for stage (#918)
    * New deploy.dev_CZ.yml
    
    * Stores check.
    
    * Changes.
    
    * Working.
    
    * New store config.
    
    * Changes.
    
    * New store config.
    
    * Hello World Testing.
    
    * new pop-ui-login-form module is created with .twig, .ts, and .scss files with the same names, and implementation of a opening of a right canvas window with the screenset for Login
    
    * fixed generating the csrf token on login form
    
    * New deploy.dev_CZ.yml
    
    * Stores check.
    
    * Changes.
    
    * Working.
    
    * New store config.
    
    * Changes.
    
    * New store config.
    
    * GSOA API implementation
    
    * Added logic for login modal
    
    * GSOA API implementation
    
    * Removed login action from side-drawer.twig
    
    * Changed type email in popup-ui-login-form.twig
    
    * GSOA API implementation
    
    * Upgrading to new version of symfony. Updating kernel version.
    
    * GSOA API implementation
    
    * Fixing RabbitMQ bug.
    
    * GSOA API implementation
    
    * Adding new custom tax for CZ store.
    
    * GSOA API implementation and data import
    
    * Creating full import yml file.
    
    * Changes in deploy.dev.yml for DE project.
    
    * Added deploy.iglobuscz-staging.yml for CZ project
    
    * Added code bucket variable for DE staging. Updating docker image file.
    
    * GSOA API implementation, data import and refactoring
    
    * Refactoring
    
    * Travis configuration
    
    * Travis configuration
    
    * Travis configuration
    
    * PR changes.
    
    * Travis fix.
    
    * Aditional changes RUE.
    
    * Aditional changes RUE.
    
    * Travis and code review
    
    * Travis and code review
    
    * Travis fix.
    
    * Product-location fix.
    
    * Travis connection
    
    * Trying to change travis file in order for unit tests to pass.
    
    * Reverting travis.yml to OPA stores.
    
    * PR fixes.
    
    * CodeBucketsPlugin
    
    * Gsoa login customer
    
    * Gsoa login customer
    
    * Travis fix
    
    * Changed default store.
    
    * Changed default store.
    
    * Category import
    
    * Change in deploy file.
    
    * Change in staging deploy file.
    
    * Added prod install script for CZ.
    
    * Adding DB params to config_default-staging.php
    
    Co-authored-by: jmedved <jmedved85@gmail.com>
    Co-authored-by: mjelinekkps <marin.jelinek@kps.com>
    Co-authored-by: Gordana Zeljezic <gordana.zeljezic@kps.com>

--- config/Shared/config_default-staging.php
+++ config/Shared/config_default-staging.php
@@ -37,3 +37,17 @@ $config[LogConstants::LOG_FILE_PATH_GLUE] = 'php://stdout';
 $config[LogConstants::EXCEPTION_LOG_FILE_PATH_YVES] = 'php://stderr';
 $config[LogConstants::EXCEPTION_LOG_FILE_PATH_ZED] = 'php://stderr';
 $config[LogConstants::EXCEPTION_LOG_FILE_PATH_GLUE] = 'php://stderr';
+
+/* Database */
+$config[PropelConstants::USE_SUDO_TO_MANAGE_DATABASE] = false;
+$config[PropelConstants::PROPEL_DEBUG] = false;
+
+$config[PropelOrmConstants::PROPEL_SHOW_EXTENDED_EXCEPTION] = (bool)getenv('SPRYKER_DEBUG_ENABLED');
+$config[PropelConstants::ZED_DB_USERNAME] = getenv('SPRYKER_DB_USERNAME');
+$config[PropelConstants::ZED_DB_PASSWORD] = getenv('SPRYKER_DB_PASSWORD');
+$config[PropelConstants::ZED_DB_HOST] = getenv('SPRYKER_DB_HOST');
+$config[PropelConstants::ZED_DB_PORT] = getenv('SPRYKER_DB_PORT');
+$config[PropelConstants::ZED_DB_ENGINE] = strtolower(getenv('SPRYKER_DB_ENGINE') ?: '') ?: PropelConfig::DB_ENGINE_PGSQL;
+$config[PropelQueryBuilderConstants::ZED_DB_ENGINE] = $config[PropelConstants::ZED_DB_ENGINE];
+$config[PropelConstants::ZED_DB_DATABASE] = getenv('SPRYKER_DB_DATABASE');
+/* End Database */
