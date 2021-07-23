e7a5ff039d7d540de547c05061b5eedcb62f9070
Marin Jelinek <74537137+mjelinekkps@users.noreply.github.com>
2021-07-23 12:25:03 +0200

    OS11SPRYKE-846 Reset pick zone price update (#1059)
    * OS11SPRYKE-846 Reset pick zone price update
    
    * OS11SPRYKE-846 Reset pick zone price update

--- src/Pyz/Zed/Sales/Communication/Plugin/Oms/Command/ResetPickZoneCommandPlugin.php
+++ src/Pyz/Zed/Sales/Communication/Plugin/Oms/Command/ResetPickZoneCommandPlugin.php
@@ -10,6 +10,8 @@ namespace Pyz\Zed\Sales\Communication\Plugin\Oms\Command;
 use Generated\Shared\Transfer\OrderPickingBlockTransfer;
 use Generated\Shared\Transfer\OrderUpdateRequestTransfer;
 use Orm\Zed\Sales\Persistence\SpySalesOrder;
+use PDO;
+use Propel\Runtime\Propel;
 use Pyz\Shared\Oms\OmsConfig;
 use Spryker\Zed\Kernel\Communication\AbstractPlugin;
 use Spryker\Zed\Oms\Business\Util\ReadOnlyArrayObject;
@@ -44,10 +46,56 @@ class ResetPickZoneCommandPlugin extends AbstractPlugin implements CommandByOrde
         $orderUpdateRequestTransfer = (new OrderUpdateRequestTransfer())
             ->setStoreStatus(OmsConfig::STORE_STATE_READY_FOR_PICKING);
 
+        $itemsIds = [];
+        foreach ($orderItems as $item) {
+            $itemsIds[] = $item->getIdSalesOrderItem();
+        }
+
+        $query = 'update spy_sales_order_item ssoiUp
+                    inner join
+                    (
+                             select ssoi.sku,
+                                    price,
+                                    new_weight,
+                                    price_per_kg,
+                                    gross_price,
+                                    price_to_pay_aggregation,
+                                    row_number() over (partition by sku order by new_weight) as rownum
+                             from spy_sales_order_item ssoi
+                             inner join (select fk_sales_order, sku from spy_sales_order_item where id_sales_order_item in (' . implode(",", $itemsIds) . ') group by fk_sales_order, sku) ss on ssoi.fk_sales_order = ss.fk_sales_order and ssoi.sku = ss.sku
+                             where not price_per_kg is null
+                             group by ssoi.sku, price, new_weight, price_per_kg, gross_price, price_to_pay_aggregation
+                    ) items on ssoiUp.sku = items.sku and items.rownum = 1
+                    set ssoiUp.price = items.price,
+                        ssoiUp.new_weight = null,
+                        ssoiUp.gross_price = items.gross_price,
+                        ssoiUp.price_to_pay_aggregation = items.price_to_pay_aggregation
+                    where ssoiUp.fk_sales_order = ' . $orderEntity->getIdSalesOrder();
+        $this->execQuery($query, false);
+
         $this->getFactory()->getMerchantSalesOrderFacade()->updateOrderWithOrderUpdateRequest(
             $orderEntity->getIdSalesOrder(),
             $orderUpdateRequestTransfer
         );
+
+        return [];
+    }
+
+    /**
+     * @param string $sql
+     * @param bool $doFetch
+     *
+     * @return array
+     */
+    private function execQuery(string $sql, bool $doFetch = true): array
+    {
+        $connection = Propel::getConnection();
+        $statement = $connection->prepare($sql);
+        $statement->execute();
+
+        if ($doFetch) {
+            return $statement->fetchAll(PDO::FETCH_NAMED);
+        }
 
         return [];
     }
