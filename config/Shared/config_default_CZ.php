398c2ac18a7b1e1abd294542364418c012028d5d
dkrizanec <61667117+dkrizanec@users.noreply.github.com>
2021-08-23 08:59:21 +0200

    OS11Spryke-917 adjust sftp for cz (#1175)
    * FTP credentials for CZ.
    
    * Adding new SFTP password.

--- config/Shared/config_default_CZ.php
+++ config/Shared/config_default_CZ.php
@@ -1,12 +1,46 @@
 <?php
 
+use Pyz\Service\FlysystemSftpFileSystem\Plugin\Flysystem\SftpFileSystemBuilderPlugin;
 use Pyz\Shared\Application\ApplicationConstants;
 use Pyz\Shared\CodeBuckets\CodeBucketConstants;
 use Pyz\Shared\GoogleTagManager\GoogleTagManagerConstants;
 use Pyz\Shared\GsoaRestApiClient\Provider\TokenProvider;
 use Pyz\Shared\ProductImage\ProductImageConstants;
 use Pyz\Shared\Store\StoreConstants;
+use Spryker\Service\FlysystemLocalFileSystem\Plugin\Flysystem\LocalFilesystemBuilderPlugin;
+use Spryker\Shared\FileSystem\FileSystemConstants;
 use Spryker\Shared\Synchronization\SynchronizationConstants;
+
+
+// ---------- FileSystem
+$config[FileSystemConstants::FILESYSTEM_SERVICE] = [
+    'files' => [
+        'sprykerAdapterClass' => LocalFilesystemBuilderPlugin::class,
+        'root' => APPLICATION_ROOT_DIR . '/data/' . APPLICATION_STORE . '/media/',
+        'path' => 'files/',
+    ],
+    'cashier_order_local' => [
+        'sprykerAdapterClass' => LocalFilesystemBuilderPlugin::class,
+        'root' => APPLICATION_ROOT_DIR . '/data/' . APPLICATION_STORE . '/export/',
+        'path' => 'files/',
+    ],
+//    'globus_sftp' => [
+//        'sprykerAdapterClass' => SftpFileSystemBuilderPlugin::class,
+//        'host' => getenv('GLOBUS_SFTP_HOST'),
+//        'port' => getenv('GLOBUS_SFTP_PORT'),
+//        'username' => getenv('GLOBUS_SFTP_USERNAME'),
+//        'password' => getenv('GLOBUS_SFTP_PASSWORD'),
+//        'root' => getenv('GLOBUS_SFTP_ROOT'),
+//    ],
+    'globus_sftp' => [
+        'sprykerAdapterClass' => SftpFileSystemBuilderPlugin::class,
+        'host' => 'ftp.globus.cz',
+        'port' => '22',
+        'username' => 'spryker',
+        'password' => '8U65f$b_p6vLNMc',
+        'root' => '/',
+    ],
+];
 
 $config[StoreConstants::SAP_STORE_ID_TO_STORE_MAP] = [
     4012 => 'OPA',
