b77324de2f47fe3d13a0e27d1b13eb07f629e9a8
dkrizanec <domagoj.krizanec@gmail.com>
2021-09-20 14:55:55 +0200

    Revert "Changing height of the images."
    This reverts commit 304669dc3a2d6785c4f1b622f36866dfcf87bd14.

--- src/Pyz/Yves/ShopUi/Theme/default/components/molecules/lazy-image/lazy-image.ts
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/lazy-image/lazy-image.ts
@@ -2,48 +2,22 @@ import Component from 'ShopUi/models/component';
 
 export default class LazyImage extends Component {
     protected image: HTMLElement;
-    protected imageSource: string;
 
     protected readyCallback(): void {
         this.image = <HTMLElement>this.getElementsByClassName(this.imageClass)[0];
-        this.imageSource = this.image.src;
+
         this.mapEvents();
     }
 
     protected mapEvents(): void {
         this.removeInvisibleClass();
-        this.setCorrectHeightClassForImage(this.imageSource);
-
     }
 
     protected removeInvisibleClass(): void {
         this.image.classList.remove('is-invisible');
     }
 
-    protected setCorrectHeightClassForImage(url: string): void
-    {
-        this.getWidthAndHeightOfImageFromSrcUrl(url);
-    }
-
-    protected getWidthAndHeightOfImageFromSrcUrl(url: string): void
-    {
-        let img = new Image();
-        var self = this;
-        img.addEventListener("load", function (){
-             if(this.width != this.height){
-                 self.removeDefaultHeightAttribute()
-             }
-        });
-        img.src = url;
-    }
-
-    protected removeDefaultHeightAttribute(): void{
-        this.image.parentElement.classList.remove('lazy-image');
-        this.image.classList.remove('js-lazy-image__content');
-    }
-
     protected get imageClass(): string {
         return `${this.jsName}__content`;
     }
-
 }
