bb363d6c0149d01b70aeaaf67be8432558ef1bf5
dkrizanec <61667117+dkrizanec@users.noreply.github.com>
2021-10-06 17:43:30 +0200

    Adding proper constants. (#1348)

--- config/Shared/config_default_CZ.php
+++ config/Shared/config_default_CZ.php
@@ -35,6 +35,44 @@ $config[StoreConstants::STORE_NAMES] = [
     'CCM' => 'Černý Most',
 ];
 
+// ---------- FileSystem
+$config[FileSystemConstants::FILESYSTEM_SERVICE] = [
+    'files' => [
+        'sprykerAdapterClass' => LocalFilesystemBuilderPlugin::class,
+        'root' => APPLICATION_ROOT_DIR . '/data/' . APPLICATION_STORE . '/media/',
+        'path' => 'files/',
+    ],
+    'cashier_order_local' => [
+        'sprykerAdapterClass' => LocalFilesystemBuilderPlugin::class,
+        'root' => APPLICATION_ROOT_DIR . '/data/' . APPLICATION_STORE . '/export/',
+        'path' => 'files/',
+    ],
+//    'globus_sftp' => [
+//        'sprykerAdapterClass' => SftpFileSystemBuilderPlugin::class,
+//        'host' => getenv('GLOBUS_SFTP_HOST'),
+//        'port' => getenv('GLOBUS_SFTP_PORT'),
+//        'username' => getenv('GLOBUS_SFTP_USERNAME'),
+//        'password' => getenv('GLOBUS_SFTP_PASSWORD'),
+//        'root' => getenv('GLOBUS_SFTP_ROOT'),
+//    ],
+    'globus_sftp' => [
+        'sprykerAdapterClass' => SftpFileSystemBuilderPlugin::class,
+        'host' => 'ftp.globus.cz',
+        'port' => '22',
+        'username' => 'spryker',
+        'password' => '8U65f$b_p6vLNMc',
+        'root' => '/home/spryker/',
+    ],
+];
+
+// ----------- GSOA API
+$config[ApiClient::GSOA_ROOT_URL] = 'https://gapi.globus.cz';
+$config[TokenProvider::GSOA_CLIENT_ID] = "webAppSpryker";
+$config[TokenProvider::GSOA_CLIENT_SECRET] = "1A5CE779-02EA-42A5-959C-9909C2C11666";
+
+$config[ProductImageConstants::IMAGES_HOST_URL] = 'https://gapi.globus.cz/OnlineAsset/3/asset?assetID=';
+
+
 // ----------- Google Tag Manager
 $config[GoogleTagManagerConstants::CONTAINER_ID] = 'GTM-PPGX4LT';
 
