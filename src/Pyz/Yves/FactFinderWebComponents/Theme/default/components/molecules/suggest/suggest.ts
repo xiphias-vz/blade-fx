9c4e72d19c818e06361e55d90ac3a2280ac19bcd
MihaelBlazek <66123456+MihaelBlazek@users.noreply.github.com>
2021-12-27 09:34:58 +0100

    Os11 spryke 1489 tl (#1665)
    * Added icon add 2 cart
    
    * Suggest Add2Cart
    
    * Add to cart from suggest search
    
    * Reverting back the deploy file because the change will break local environment.
    
    Co-authored-by: tlevkus <tomislav.levkus@xiphias.hr>
    Co-authored-by: Mihael Blazek <mihael.blazek@kps.com>
    Co-authored-by: dkrizanec <domagoj.krizanec@gmail.com>

--- src/Pyz/Yves/FactFinderWebComponents/Theme/default/components/molecules/suggest/suggest.ts
+++ src/Pyz/Yves/FactFinderWebComponents/Theme/default/components/molecules/suggest/suggest.ts
@@ -1,13 +1,30 @@
 import Component from 'ShopUi/models/component';
+import FlashMessage from 'ShopUi/components/molecules/flash-message/flash-message';
+import $ from 'jquery/dist/jquery';
 
 export default class Suggest extends Component {
     clearButton: HTMLElement;
+    btnCallTs: HTMLElement;
     searchInput: HTMLInputElement;
+    suggestAddToCartForm: HTMLFormElement;
     currentSearchValue: string;
+    protected addToCartButton: $;
+    protected flashMessages: FlashMessage[];
+    protected messagesTextHolders: HTMLElement[];
+    protected notificationArea: HTMLElement;
+
 
     protected readyCallback(): void {
         this.clearButton = <HTMLElement>document.getElementsByClassName(this.clearButtonClassName)[0];
         this.searchInput = <HTMLInputElement>document.getElementsByClassName(this.searchInputClassName)[0];
+        this.addToCartButton = <HTMLElement>$(document).find(this.getAddToCartBtn);
+        this.suggestAddToCartForm = <HTMLFormElement>document.querySelector(this.getAddToCartForm);
+        this.flashMessages = <FlashMessage[]>Array.from(this.getElementsByClassName(this.flashMessagesClass));
+        this.notificationArea = <HTMLElement>this.getElementsByClassName(this.notificationAreaClass)[0];
+        this.messagesTextHolders = <HTMLElement[]>Array.from(
+            this.getElementsByClassName(this.messagesTextHolderClass)
+        );
+        this.btnCallTs = <HTMLFormElement>document.querySelector("#callTs")
         this.changesInInputField();
         this.load();
         this.mapEvents();
@@ -16,12 +33,78 @@ export default class Suggest extends Component {
     protected load() {
        window.addEventListener('load', () => {
            this.classList.remove(`suggest__hide`);
-       })
+       });
     }
 
     protected mapEvents(): void {
         this.clearButton.addEventListener('click', () => this.clearSearchField());
+        this.btnCallTs.addEventListener('click', () => this.submitToCartButton());
         this.searchInput.addEventListener('keyup', () => this.changesInInputField());
+    }
+
+    protected submitToCartButton(): void {
+        const that = this;
+
+        let formData = new FormData(this.suggestAddToCartForm);
+        let ajax_url = $("#addAjaxUrl").val();
+        let isCartButtonClicked = document.getElementById("is-cart-button-clicked");
+        isCartButtonClicked.value = 'y';
+
+            $.ajax(ajax_url, {
+                type: 'POST',
+                dataType: 'json',
+                data: {
+                    myToken: formData.get('token'),
+                    myOperation: "ADD",
+                    myQuantity: 1
+                },
+                success(data, status, xhr) {
+                    if(data.error !== '') {
+                        that.setMessages(data.error);
+                        that.showMessages();
+                        return;
+                    }
+
+                    that.replaceQuantity(data.quantity);
+                    that.replaceAmount(data.amount);
+                    isCartButtonClicked.value = 'n';
+                },
+                error(jqXhr, textStatus, errorMessage) {
+                    that.setMessages(jqXhr.error);
+                    that.showMessages();
+                }
+            });
+
+            document.getElementsByClassName("suggest-search__input")[0].focus();
+        }
+
+    protected replaceQuantity(quantity: number): void {
+        let quantityElements = $('.js-ajax-add-to-cart__quantity');
+        let i;
+        for (i=0; i < quantityElements.length; i++){
+            quantityElements[i].innerHTML = quantity;
+        }
+    }
+
+    protected replaceAmount(amount: number): void {
+        let amountElements = $('.js-ajax-add-to-cart__amount');
+        let i;
+        for (i=0; i < amountElements.length; i++){
+            amountElements[i].innerHTML = amount;
+        }
+    }
+
+    protected showMessages(): void {
+        this.notificationArea.classList.remove("is-hidden");
+        this.flashMessages.forEach(flashMessageItem => {
+            flashMessageItem.showFor(flashMessageItem.defaultDuration);
+        });
+    }
+
+    protected setMessages(message: string): void {
+        this.messagesTextHolders.forEach((messageItem: HTMLElement) => {
+            messageItem.innerHTML = message;
+        });
     }
 
     protected clearSearchField(): void {
@@ -48,5 +131,25 @@ export default class Suggest extends Component {
 
     protected get searchInputClassName(): string {
         return 'searchbox__input';
+    }
+
+    protected get getAddToCartBtn(): string {
+        return '.suggest__button-suggest-cart';
+    }
+
+    protected get getAddToCartForm(): string {
+        return '#submit-suggested-search-product';
+    }
+
+    protected get flashMessagesClass(): string {
+        return `js-ajax-add-to-cart__flash-message`;
+    }
+
+    protected get messagesTextHolderClass(): string {
+        return `js-ajax-add-to-cart__message-text`;
+    }
+
+    protected get notificationAreaClass(): string {
+        return `js-ajax-add-to-cart__notification-area`;
     }
 }
