3f3ac2de422156657b0f3d96ec0277b0864cec90
Marin Jelinek <74537137+mjelinekkps@users.noreply.github.com>
2022-01-04 13:39:01 +0100

    OS11SPRYKE-961 FF product export fix (#1686)
    * OS11SPRYKE-1513 browser back
    
    * OS11SPRYKE-961 product export fix

--- src/Pyz/Zed/FactFinderExport/Communication/Console/FactFinderConsole.php
+++ src/Pyz/Zed/FactFinderExport/Communication/Console/FactFinderConsole.php
@@ -194,18 +194,18 @@ FROM spy_product sp
     LEFT OUTER JOIN spy_category sc on spc.fk_category = sc.id_category AND sc.is_active = 1 AND sc.is_in_menu = 1
     LEFT OUTER JOIN spy_category_attribute sca on sc.id_category = sca.fk_category
     LEFT OUTER JOIN spy_category_node scn on sc.id_category = scn.fk_category
-    LEFT OUTER JOIN spy_category sc2 on scn.fk_parent_category_node = sc2.id_category AND sc2.is_active = 1 AND sc2.is_in_menu = 1
+    LEFT OUTER JOIN spy_category_node scn2 on scn.fk_parent_category_node = scn2.id_category_node
+    LEFT OUTER JOIN spy_category_node scn3 on scn2.fk_parent_category_node = scn3.id_category_node
+    LEFT OUTER JOIN spy_category_node scn4 on scn3.fk_parent_category_node = scn4.id_category_node
+    LEFT OUTER JOIN spy_category_node scn5 on scn4.fk_parent_category_node = scn5.id_category_node
+    LEFT OUTER JOIN spy_category sc2 on scn2.fk_category = sc2.id_category AND sc2.is_active = 1 AND sc2.is_in_menu = 1
+    LEFT OUTER JOIN spy_category sc3 on scn3.fk_category = sc3.id_category  AND sc3.is_active = 1 AND sc3.is_in_menu = 1
+    LEFT OUTER JOIN spy_category sc4 on scn4.fk_category = sc4.id_category  AND sc4.is_active = 1 AND sc4.is_in_menu = 1
+    LEFT OUTER JOIN spy_category sc5 on scn5.fk_category = sc5.id_category  AND sc5.is_active = 1 AND sc5.is_in_menu = 1
     LEFT OUTER JOIN spy_category_attribute sca2 on sc2.id_category = sca2.fk_category
-    LEFT OUTER JOIN spy_category_node scn2 on sc2.id_category = scn2.fk_category
-    LEFT OUTER JOIN spy_category sc3 on scn2.fk_parent_category_node = sc3.id_category AND sc3.is_active = 1 AND sc3.is_in_menu = 1
     LEFT OUTER JOIN spy_category_attribute sca3 on sc3.id_category = sca3.fk_category
-    LEFT OUTER JOIN spy_category_node scn3 on sc3.id_category = scn3.fk_category
-    LEFT OUTER JOIN spy_category sc4 on scn3.fk_parent_category_node = sc4.id_category AND sc4.is_active = 1 AND sc4.is_in_menu = 1
     LEFT OUTER JOIN spy_category_attribute sca4 on sc4.id_category = sca4.fk_category
-    LEFT OUTER JOIN spy_category_node scn4 on sc4.id_category = scn4.fk_category
-    LEFT OUTER JOIN spy_category sc5 on scn4.fk_parent_category_node = sc5.id_category AND sc5.is_active = 1 AND sc5.is_in_menu = 1
     LEFT OUTER JOIN spy_category_attribute sca5 on sc5.id_category = sca5.fk_category
-    LEFT OUTER JOIN spy_category_node scn5 on sc5.id_category = scn5.fk_category
  WHERE sp.is_active = 1
  GROUP BY sp.sku, sp.product_number";
     }
