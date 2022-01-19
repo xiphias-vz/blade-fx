cbe386698fc81bb8ee8edff0c846afda307d59f3
Tomislav Levkuš <93911983+TomislavLevkus@users.noreply.github.com>
2022-01-19 15:30:27 +0100

    Sale category fix (#1741)

--- src/Pyz/Yves/ContentNavigationWidget/Theme/default/components/molecules/navigation-category/navigation-category.ts
+++ src/Pyz/Yves/ContentNavigationWidget/Theme/default/components/molecules/navigation-category/navigation-category.ts
@@ -4,6 +4,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
         return;
     }
     let currentUrl = location.pathname;
+    let akceCheckbox = document.querySelector(".checkbox__input");
 
     if(currentUrl.includes('/de/outlet')) {
         const filterCategoryLink = filterSection.querySelectorAll('.filter-category .filter-category__item--level-1 .filter-category__link');
@@ -13,12 +14,7 @@ window.addEventListener('DOMContentLoaded', (e) => {
             filterCategoryLink[i].setAttribute('data-url', categorySalesLink);
         }
     }
-    else if(currentUrl.includes('/cs/outlet')) {
-        const filterCategoryLink = filterSection.querySelectorAll('.filter-category .filter-category__item--level-1 .filter-category__link');
-        for(let i = 0; i < filterCategoryLink.length; i++) {
-            let urlCategoryLink = filterCategoryLink[i].getAttribute('data-url');
-            let categorySalesLink = '/cs/outlet'+ urlCategoryLink.substring(urlCategoryLink.indexOf("/") + 4);
-            filterCategoryLink[i].setAttribute('data-url', categorySalesLink);
-        }
+    else if(currentUrl.includes('/cs/outlet') && akceCheckbox.checked == false) {
+        akceCheckbox.checked = true;
     }
 })
