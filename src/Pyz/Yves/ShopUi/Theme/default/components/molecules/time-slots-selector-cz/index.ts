5877b56e96fa3c941a2758f7816d087fe45cee0c
MihaelBlazek <66123456+MihaelBlazek@users.noreply.github.com>
2022-05-13 16:00:15 +0200

    OS11SPRYKE-1744[5.9 CodeX] (#2077)
    * Izmjene na timeslots.
    
    * Changing layout for timeslots.
    
    * Layout changes.
    
    * Revert "Layout changes."
    
    This reverts commit 08fc0292e744efe918759f1d83c423f6d72b72fc.
    
    * Revert "Revert "Layout changes.""
    
    This reverts commit 52dcc6ad7458267d8929fd7b8aeae1825b761e8a.
    
    * Checkout timeslots
    
    * Checkout timeslots
    
    * Styling timeslots for CZ shop
    
    * time-slots styling for DE and CZ
    
    * Added Styles for CZ
    
    Co-authored-by: TomislavLevkus <tomislav.levkus@xiphias.hr>
    Co-authored-by: Domagoj Križanec <domagoj.krizanec@gmail.com>
    Co-authored-by: MisoMikulaj <misomikulaj94@gmail.com>
    Co-authored-by: ajelinic <andrej.jelinic@gmail.com>
    Co-authored-by: tlevkus <filip.gojkovic@kps.com>

---
+++ src/Pyz/Yves/ShopUi/Theme/default/components/molecules/time-slots-selector-cz/index.ts
@@ -0,0 +1,3 @@
+import './time-slots-selector-cz.scss';
+import register from 'ShopUi/app/registry';
+export default register('time-slots-selector-cz', () => import(/* webpackMode: "lazy" */'./time-slots-selector-cz'));
