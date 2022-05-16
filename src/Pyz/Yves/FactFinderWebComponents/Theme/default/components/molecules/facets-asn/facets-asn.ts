227f5c2f87b897e2d17e48cb72f89f0e43b7e088
Tomislav Levkuš <93911983+TomislavLevkus@users.noreply.github.com>
2022-05-16 10:50:12 +0200

    OS11SPRYKE-1886 [5.9] (#2101)
    * Changes to scss.
    
    * Revert "OS11SPRYKE-1703 Information for medicines [5.7] (#1948)"
    
    This reverts commit 3af6fc7cf593b06ecc6a1b35e37be870c9203bd0.
    
    * commitanje promijenjenih file-ova
    
    * commitanje promijenjenih file-ova
    
    * Style changes (color, positioning, box-sizing)
    
    * Frontend adjustments
    
    * Style changes
    
    * Box sizing style changes
    
    * [OS11SPRYKE-1737] - added styles for footer
    
    * Changed color, positioning, box-model, font-size, icons ...
    
    * Adjusting style
    
    * Adjusting style for CZ
    
    * Styling checkout/cart page.
    
    * Adjusting style for CZ, eye-icon, buttons, spacing...
    
    * Fixing Eye icon for DE
    
    * styling
    
    * [OS11SPRYKE-1743] - added styles for checkout>step2>abholstation
    
    * Adjusted CSS an JS for password visibility icon
    
    * Removed unnecessary code
    
    * [OS11SPRYKE-1737] - added styles for footer
    
    * [OS11SPRYKE-1743] - added styles
    
    * [OS11SPRYKE-1737] added stlyes for CZ
    
    * Style changes for CZ & DE
    
    * Setting cursor pointer for icon
    
    * Setting cursor pointer for icon on DE
    
    * [OS11SPRYKE-1743] added stlyes for CZ
    
    * [OS11SPRYKE-1737] - added styles
    
    * Cleaning & refactoring code, fixed icon positioning, adjusted color names
    
    * Cleaning and refactoring code
    
    * Cleaning and refactoring code
    
    * search-suggest component adjusted on DE (desktop & mobile)
    
    * [OS11SPRYKE-1742] - Added styles for tablet
    
    * product tiles styles mobile view
    
    * [OS11SPRYKE-1742] - Added styles for tablet
    
    * Styling error 404 page
    
    * [OS11SPRYKE-1737] - footer styles fix
    
    * [OS11SPRYKE-1742] - added styles for CZ
    
    * [OS11SPRYKE-1737] - footer styles fix
    
    * [OS11SPRYKE-1742] - added styles for CZ
    
    * [OS11SPRYKE-1742] - added styles for CZ
    
    * [OS11SPRYKE-1743] - bugfix
    
    * suggest search css and functionality adjusted on CZ codeBucket
    
    * Customer navigation frontend adjustments
    
    * Code adjustments
    
    * fixes for canceled and partialy picked items
    
    * adjusting css and suggest functionality on DE codeBucket
    
    * 1165 fix.
    
    * Slider fix.
    
    * change colors
    
    * change colors
    
    * change colors
    
    * error page redesign
    
    * DE Navigation frontend adjustments
    
    * Navigation menu hover adjustments
    
    * Link fix & frontend adjustments
    
    * Removing unnecessary code
    
    * Added icon logic in typescript, removed unnecessary css classes
    
    Co-authored-by: Marin Jelinek <74537137+mjelinekkps@users.noreply.github.com>
    Co-authored-by: ekres1 <kres.edo@gmail.com>
    Co-authored-by: MisoMikulaj <misomikulaj94@gmail.com>
    Co-authored-by: ajelinic <andrej.jelinic@gmail.com>
    Co-authored-by: fgojkovic94 <filip.gojkovic@kps.com>
    Co-authored-by: jmedved <jmedved85@gmail.com>
    Co-authored-by: Domagoj Križanec <domagoj.krizanec@gmail.com>

---
+++ src/Pyz/Yves/FactFinderWebComponents/Theme/default/components/molecules/facets-asn/facets-asn.ts
@@ -0,0 +1,97 @@
+import Component from 'ShopUi/models/component';
+
+export default class FacetsAsn extends Component
+{
+    protected asnPrice: HTMLElement;
+    private defaultClassName: string = 'icon-globus-leaf-default-md';
+    private categoryClass: string = 'category-filter-style';
+    private categoryClassDiv: string = 'category-filter-style-div';
+    protected classData = {
+        'Fachtmetzgerei': 'icon-butcher-md',
+        'Süßes & Salziges': 'icon-sweet-and-savoury-md',
+        'Wurst & Käse': 'icon-sausage-and-cheese-md',
+        'Bäckerei': 'icon-baker-md',
+        'Fleich & Fisch': 'icon-meat-sausages-fish-md',
+        'Tiefkühl': 'icon-ready-meals-md',
+        'Angebote': 'icon-offer-md',
+        // koje sam ja našel u asn response-u za DevSpryker FF kanal ↓
+        'Obst & Gemüse': 'icon-fruit-and-vegetables-md',
+        'Spiel- & Schreibwaren': 'icon-games-md',
+        'Spiel & Schreibwaren': 'icon-games-md',
+        'Körperpflege & Kosmetik': 'icon-cosmetics-md',
+        'Frische Produkte': 'icon-fresh-product-md',
+        'Vorratsschrank': 'icon-pantry-md',
+        'Freizeit, Tier & Auto': 'icon-animals-md',
+        'Baby & Schwangerschaft': 'icon-baby-md',
+        'Elektronik': 'icon-electronics-md',
+        'Textil & Schmuck': 'icon-clothes-md',
+        'Getränke': 'icon-drinks-md',
+        'Fertiggerichte & Tiefkühl': 'icon-ready-meals-md',
+        'Fisch & Fleisch': 'icon-meat-md',
+        'Haushalt': 'icon-household-md'
+    };
+    protected map: Map<string, string>;
+
+    protected init() {
+        super.init();
+        this.map = new Map<string, string>(Object.entries(this.classData));
+
+        if (document.readyState === 'interactive' || document.readyState === 'complete') {
+            window.addEventListener('DOMNodeInserted', nodeInsertedEvent => {
+                if (nodeInsertedEvent.relatedNode.localName === 'ff-asn-group-element') {
+                    const span = nodeInsertedEvent.relatedNode.querySelector('span');
+                    if (span !== undefined && span !== null) {
+                        if (span.classList.contains('filterName')) {
+                            const className = this.getClassName(span.innerText);
+                            if (!nodeInsertedEvent.relatedNode.classList.contains('clusterLevel1') &&
+                                !nodeInsertedEvent.relatedNode.classList.contains('clusterLevel2') &&
+                                !nodeInsertedEvent.relatedNode.classList.contains('clusterLevel3'))
+                            {
+                                this.addClasses(span, className, 'span');
+                            }
+                        }
+                    } else {
+                        if (nodeInsertedEvent.relatedNode.hasAttribute('selected')) {
+                            const parentDiv = nodeInsertedEvent.relatedNode.querySelector('div[slot=selected]');
+                            if (parentDiv !== undefined && parentDiv !== null) {
+                                const className = this.getClassName(parentDiv.innerText);
+                                this.addClasses(parentDiv, className, 'div');
+                            }
+                        }
+                    }
+                } else if (nodeInsertedEvent.relatedNode.localName === 'ff-navigation-item') {
+                    let anchor = nodeInsertedEvent.relatedNode.querySelector('.navigation-icon');
+                    if (anchor !== undefined && anchor !== null) {
+                        const className = this.getClassName(anchor.innerText.trim());
+                        if (nodeInsertedEvent.relatedNode.hasAttribute('cluster-level')) {
+                            if (nodeInsertedEvent.relatedNode.getAttribute('cluster-level') === '0') {
+                                this.addClasses(anchor, className, 'span');
+                            }
+                        }
+                    }
+                }
+            });
+        }
+    }
+
+    protected readyCallback() {}
+
+    private getClassName(text: string) {
+        text = text.trim();
+        let className = this.map.get(text);
+        if (className === undefined || className === null || className.length < 1) {
+            className = this.defaultClassName;
+        }
+
+        return className;
+    }
+
+    private addClasses(element: HTMLElement, className: string, flag: string) {
+        element.classList.add(className);
+        if (flag === 'div') {
+            element.classList.add(this.categoryClassDiv);
+        } else {
+            element.classList.add(this.categoryClass);
+        }
+    }
+}
--- src/Pyz/Yves/FactFinderWebComponents/Theme/default/components/molecules/facets-asn/index.ts
+++ src/Pyz/Yves/FactFinderWebComponents/Theme/default/components/molecules/facets-asn/index.ts
@@ -1,1 +1,3 @@
 import './facets-asn.scss';
+import register from 'ShopUi/app/registry';
+export default register('facets-asn', () => import(/* webpackMode: "eager" */'./facets-asn'));
