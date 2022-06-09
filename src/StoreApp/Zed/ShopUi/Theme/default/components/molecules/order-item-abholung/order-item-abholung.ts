3aef72e956eb3613841201cd65ce82ed0988c333
Andrej Jelinic <47660412+ajelinic@users.noreply.github.com>
2022-06-09 08:48:06 +0200

    OS11SPRYKE-1958[6.0] (#2365)
    * [OS11SPRYKE-2000] - Added front end styles for details view storeapp
    
    * css fix
    
    * Order list on abholung design
    
    * Fixing some styles, whole order clickable
    
    * Styling of order pickup view
    
    Co-authored-by: ekres1 <kres.edo@gmail.com>

--- src/StoreApp/Zed/ShopUi/Theme/default/components/molecules/order-item-abholung/order-item-abholung.ts
+++ src/StoreApp/Zed/ShopUi/Theme/default/components/molecules/order-item-abholung/order-item-abholung.ts
@@ -17,6 +17,11 @@ export default class OrderItemAbholung extends Component {
     protected lastOrderInQueueInput: HTMLInputElement;
     protected isOrderInQueue: Boolean;
     protected counterChecked = 0;
+    protected customerNote: HTMLElement;
+    protected customerNoteInfo: HTMLElement;
+    protected arrowToggle: HTMLElement;
+    protected bottomInfo: HTMLElement;
+    protected detailPage: HTMLInputElement;
 
     protected readyCallback() {}
 
@@ -32,8 +37,21 @@ export default class OrderItemAbholung extends Component {
         this.orderId = <HTMLInputElement>document.querySelector('input[name="idOrder"]');
         this.isOrderInQueue = <Boolean>document.querySelector('input[id="isOrderInQueue"]');
         this.lastOrderInQueueInput = <HTMLInputElement>document.querySelector('input[id="lastOrderInQueue"]');
+        this.customerNote = <HTMLElement>this.querySelector(".customer-info__cart-note");
+        this.customerNoteInfo = <HTMLElement>this.querySelector(".customer-info__cart-note--info");
+        this.arrowToggle = <HTMLElement>this.querySelector(".customer-info__chevron");
+        this.bottomInfo = <HTMLElement>this.querySelector(".order-item-abholung__bottom-info");
+        this.detailPage = <HTMLInputElement>document.querySelector('input[class="detail-page"]');
+        this.removeHiddenClassIfCartNote();
         this.checkForWaitingQueue();
         setInterval(this.checkForWaitingQueue.bind(this), this.checkOrderInQueueDelay);
+
+        if(this.bottomInfo) {
+            if (!this.bottomInfo.childElementCount > 0) {
+                this.bottomInfo.style.padding = "0";
+            }
+        }
+
         this.mapEvents();
     }
 
@@ -48,6 +66,33 @@ export default class OrderItemAbholung extends Component {
                         that.order.style.display = "block";
                     }
                 }
+            });
+        }
+
+        if(this.customerNoteInfo) {
+            this.customerNoteInfo.addEventListener('click', function() {
+                if (this.customerNote.classList.contains(this.hiddenClass)){
+                    this.customerNote.classList.remove(this.hiddenClass)
+                    this.arrowToggle.classList.add(this.rotatedClass);
+                } else {
+                    this.customerNote.classList.add(this.hiddenClass)
+                    this.arrowToggle.classList.remove(this.rotatedClass);
+                }
+            }.bind(this));
+        }
+
+        if (this.order && this.detailPage.value !== "true") {
+            this.order.addEventListener('click', function(event){
+                let noteValue = that.customerNote.getAttribute('value');
+                event.target.classList.forEach(item => {
+                    if (!item.includes('customer-info')) {
+                        let orderForm = <HTMLFormElement>that.order.querySelectorAll('.js-submit-form-handler__form');
+                        orderForm[0].submit();
+                    } else if(item.includes('customer-info') && noteValue !== "true"){
+                        let orderForm = <HTMLFormElement>that.order.querySelectorAll('.js-submit-form-handler__form');
+                        orderForm[0].submit();
+                    }
+                });
             });
         }
 
@@ -90,12 +135,24 @@ export default class OrderItemAbholung extends Component {
         }
     }
 
+    protected removeHiddenClassIfCartNote (): void
+    {
+        let noteValue = this.customerNote.getAttribute('value');
+        if(noteValue === "true") {
+            this.customerNoteInfo.classList.remove(this.hiddenClass)
+        }
+    }
+
+
     protected checkForWaitingQueue(): void
     {
         let currentTime = new Date().getTime();
         let timeAfterWaiting = this.waitingTime.value * 1000;
         if (currentTime > timeAfterWaiting && this.waitingTime.value) {
+            this.order.classList.remove("order-item-abholung__pickup-in-queue");
             this.order.classList.add("order-item-abholung__pickup-alert");
+        } else if(this.queuedOrder && this.queuedOrder.innerText !== "") {
+            this.order.classList.add("order-item-abholung__pickup-in-queue");
         }
     }
 
@@ -139,5 +196,13 @@ export default class OrderItemAbholung extends Component {
                 }
             },
         });
+    }
+
+    get hiddenClass(): string {
+        return 'is-hidden';
+    }
+
+    get rotatedClass(): string {
+        return 'customer-info__chevron-rotated';
     }
 }
