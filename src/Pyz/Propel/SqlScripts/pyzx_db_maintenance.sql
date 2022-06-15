3c91ffc4aa139ce11700d9ac1e6ae3c7bd7c3585
Edo Kreš <95210152+ekres1@users.noreply.github.com>
2022-06-15 10:39:20 +0200

    OS11SPRYKE-1850[6.0] (#2393)
    * [OS11SPRYKE-1850] - Added new procedure and jenkins job.
    
    * [OS11SPRYKE-1850] - Updated procedure.
    
    * [OS11SPRYKE-1850] - Added new tables for optimization.

--- src/Pyz/Propel/SqlScripts/pyzx_db_maintenance.sql
+++ src/Pyz/Propel/SqlScripts/pyzx_db_maintenance.sql
@@ -3,4 +3,15 @@ CREATE OR REPLACE PROCEDURE pyzx_db_maintenance()
 BEGIN
     OPTIMIZE TABLE spy_price_product_schedule;
     OPTIMIZE TABLE spy_sales_order;
+    OPTIMIZE TABLE pyz_imp_price_product;
+    OPTIMIZE TABLE pyz_imp_stock;
+    OPTIMIZE TABLE pyz_order_picking_block;
+    OPTIMIZE TABLE pyz_picking_sales_order;
+    OPTIMIZE TABLE pyz_product_abstract_store;
+    OPTIMIZE TABLE spy_availability;
+    OPTIMIZE TABLE spy_availability_abstract;
+    OPTIMIZE TABLE spy_price_product;
+    OPTIMIZE TABLE spy_price_product_default;
+    OPTIMIZE TABLE spy_price_product_store;
+    OPTIMIZE TABLE spy_stock_product;
 END
