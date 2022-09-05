2dcf0bd59c1bca7be7ba2278ac59d83cbdb0869e
NinoHasl <nino.hasl2@gmail.com>
2022-09-05 08:49:21 +0200

    removed twig files to be manually implemented

--- src/BladeFxSpryker/Zed/Sales/Communication/Controller/ViewReportController.php
+++ src/BladeFxSpryker/Zed/Sales/Communication/Controller/ViewReportController.php
@@ -5,7 +5,7 @@
  * For full license information, please view the LICENSE file that was distributed with this source code.
  */
 
-namespace Pyz\Zed\Sales\Communication\Controller;
+namespace BladeFxSpryker\Zed\Sales\Communication\Controller;
 
 use Generated\Shared\Transfer\OrderDataTransfer;
 use Pyz\Zed\Sales\SalesConfig;
